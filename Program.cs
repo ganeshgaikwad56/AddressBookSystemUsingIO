569dbed90b7c7d3bd5b73aaac2d534132926152a
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-22 05:06:55 +0530

    Ganesh Adding program read IO file for UC13

--- Program.cs
+++ Program.cs
@@ -5,7 +5,35 @@ namespace AddressBookSystemIO
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book System Program File.");
+            string path = @"C:\Users\HP\Desktop\fellowShip\AddressBookSystemIO\AddressBook.txt";
+
+            ReadAllLines(path);
+        }
+        public static void FileExist()
+        {
+            string path = @"C:\Users\HP\Desktop\fellowShip\AddressBookSystemIO\AddressBook.txt";
+
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File Exist");
+            }
+            else
+            {
+                Console.WriteLine("Not Exist");
+            }
+        }
+
+    // IO ReadAlline UC13
+    public static void ReadAllLines(string path)
+        {
+            string[] lines;
+
+            lines = File.ReadAllLines(path);
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
         }
     }
 }

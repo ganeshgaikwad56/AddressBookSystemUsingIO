8f3d6a03bf50e0c1431e85e2745c1f8bea931b4a
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-22 05:40:43 +0530

    Ganesh Adding IO file for UC13

--- Program.cs
+++ Program.cs
@@ -7,7 +7,11 @@ namespace AddressBookSystemIO
         {
             string path = @"C:\Users\HP\Desktop\fellowShip\AddressBookSystemIO\AddressBook.txt";
 
-            ReadAllLines(path);
+            //ReadAllLines(path);
+            //ReadFromStreamReader(path);
+            WriteUsingStreamWriter(path);
+
+
         }
         public static void FileExist()
         {
@@ -35,5 +39,34 @@ namespace AddressBookSystemIO
                 Console.WriteLine(line);
             }
         }
+        public static void ReadFromStreamReader(string path)
+        {
+            using(StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s=sr.ReadLine())!=null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+        public static void WriteUsingStreamWriter(string path)
+        {
+            using(StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine("");
+                sw.WriteLine("***********New Details***********");
+                sw.WriteLine("Name :Harshal");
+                sw.WriteLine("Last Name :Deore");
+                sw.WriteLine("Adress : balaji road main raod, Nashik");
+                sw.WriteLine("City :Nashik");
+                sw.WriteLine("State :Maharashtra");
+                sw.WriteLine("zip :423402");
+                sw.WriteLine("phone :63251456");
+                sw.Close();
+                Console.WriteLine(File.ReadAllText(path));
+            }
+        }
+
     }
 }
